3b38b825f7f5142bdc10300e3fd885d6ae53d466
unknown <alldii1956@gmail.com>
2021-10-08 10:14:32 +0700

    App Kuliner

---
+++ kuliner/src/views/Foods.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="navbar">
+  <Navbar />
+
+    <div class="container">
+      <h3>Foods</h3>
+    </div>
+  
+  </div>
+  
+</template>
+
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'Foods',
+  components: {
+    Navbar
+  }
+}
+</script>
---
+++ kuliner/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="navbar">
+  <Navbar />
+
+    <div class="container">
+      <h3>Home</h3>
+    </div>
+  
+  </div>
+  
+</template>
+
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Navbar
+  }
+}
+</script>
